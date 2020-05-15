9f2e081951d66bc320e76fe39ea3a7f0b9a424f3
mikehahaha <mikehaha@163.com>
2020-05-15 23:57:20 +0800

    1

---
+++ Mike/src/com/mike/date.java
@@ -0,0 +1,13 @@
+package com.mike;
+
+import java.util.Date;
+
+public class date {
+   public static void main(String args[]) {
+       // 初始化 Date 对象
+       Date date = new Date();
+        
+       // 使用 toString() 函数显示日期时间
+       System.out.println(date.toString());
+   }
+}
